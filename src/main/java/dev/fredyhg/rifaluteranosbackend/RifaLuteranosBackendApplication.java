ca705f82ee846086eade35549604e641b683d4da
fredyhg <fredyultima2@gmail.com>
2024-05-15 22:11:41 -0300

    feat: init project

---
+++ src/main/java/dev/fredyhg/rifaluteranosbackend/RifaLuteranosBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.fredyhg.rifaluteranosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifaLuteranosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifaLuteranosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fredyhg/rifaluteranosbackend/RifaLuteranosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fredyhg.rifaluteranosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifaLuteranosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
